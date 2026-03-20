4d02687cc2f093b1af215d044bbd04d386b3cf7c
Kurochaez <pimonlak.c@ku.th>
2026-03-20 21:37:27 +0700

    change frontend

---
+++ frontend/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+function initNavbar() {
+    const links = document.querySelectorAll(".menu-item");
+    const currentPage = window.location.pathname;
+
+    links.forEach( item => {
+        item.classList.remove("menu-active");
+
+        if(currentPage.includes(item.getAttribute("href"))){
+            item.classList.add("menu-active");
+        }
+    })
+}
---
+++ frontend/js/booking.js
@@ -0,0 +1,41 @@
+const bookings = [
+  {
+    table: "A1",
+    start: 12,
+    end: 14,
+
+  },
+];
+
+function renderBookings() {
+  const rows = document.querySelectorAll(".timeline-table tbody tr");
+
+  rows.forEach((row) => {
+    const tableName = row.querySelector(".table-name").innerText;
+
+    bookings.forEach((b) => {
+      if (b.table === tableName) {
+        const slots = row.querySelectorAll(".slot");
+
+        const startIndex = b.start - 10;
+        const span = b.end - b.start;
+
+        const td = slots[startIndex];
+
+        td.colSpan = span;
+
+        td.innerHTML = `
+        <div class="booking">
+         ${b.start}:00 - ${b.end}:00
+        </div>
+        `;
+
+        for (let i = 1; i < span; i++) {
+          slots[startIndex + i].remove();
+        }
+      }
+    });
+  });
+}
+
+
